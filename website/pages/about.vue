fe5ffa0cb8c5bf24fd8913b1973a3af62b9ad874
Matt Cavanagh <maelstrome26@gmail.com>
2021-09-01 16:08:40 +0100

    Added about page

---
+++ website/pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <v-row justify="center" align="center">
+      <v-col cols="12" md="9">
+        <v-card-title class="text-h2 justify-center">
+          Humanity Deck
+        </v-card-title>
+        <v-card-text>
+              <span class="text-center">Humanity Deck was born out of the frustration with other Cards Against Humanity websites which are super clunky and frustrating to play with, often riddled with full servers or laggy gameplay. The whole point of this site is to provide a slick CAH gaming experience, using the most modern technologies available today.</span>
+        </v-card-text>
+        <v-card-actions class="justify-center">
+          <v-btn color="primary" nuxt to="/">K</v-btn>
+        </v-card-actions>
+      </v-col>
+    </v-row>
+  </section>
+</template>
--- website/pages/index.vue
+++ website/pages/index.vue
@@ -6,10 +6,10 @@
             <v-card-title class="text-h2 justify-center">
               A card game for horrible people
             </v-card-title>
-            <v-card-text>
-              <span class="text-center">
+            <v-card-text class="pb-0">
+              <p class="text-center mb-2">
                 Welcome to Humanity Deck! This website enables you to play the Cards Against Humanity game online and enjoy it with friends. This may or may not say this is a clone of the game. Whatever you wanna believe, we won't judge. That's like, your opinion man.
-              </span>
+              </p>
             </v-card-text>
             <v-card-actions class="justify-center">
               <v-btn color="primary" nuxt to="/host">Host a game!</v-btn>
