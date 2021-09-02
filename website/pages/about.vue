d59a79db6ad1b8ec554b0381e416f8bd349e979b
Matt Cavanagh <maelstrome26@gmail.com>
2021-09-03 00:25:57 +0100

    Updated about page

--- website/pages/about.vue
+++ website/pages/about.vue
@@ -6,7 +6,19 @@
           Humanity Deck
         </v-card-title>
         <v-card-text>
-              <span class="text-center">Humanity Deck was born out of the frustration with other Cards Against Humanity websites which are super clunky and frustrating to play with, often riddled with full servers or laggy gameplay. The whole point of this site is to provide a slick CAH gaming experience, using the most modern technologies available today.</span>
+          <p class="text-center">
+            Humanity Deck was born out of the frustration with other Cards
+            Against Humanity websites which are super clunky and frustrating to
+            play with, often riddled with full servers or laggy gameplay. The
+            whole point of this site is to provide a slick CAH gaming
+            experience, using the most modern technologies available today.
+          </p>
+          <p class="text-center">
+            This website is open source, you can help build it! Check out the
+            <a href="https://github.com/Maelstromeous/cards">
+              GitHub Repository </a
+            >!
+          </p>
         </v-card-text>
         <v-card-actions class="justify-center">
           <v-btn color="primary" nuxt to="/">K</v-btn>
